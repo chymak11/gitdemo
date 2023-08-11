c90a08656c474b2c4fe952c43b96b3d0e3525966
Cloud User <ec2-user@ip-172-31-91-221.ec2.internal>
2023-08-10 20:25:22 -0400

    move file

---
+++ hello.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "Welcome to landmark Technologies"
+  echo "Your are now a millionaire"
+  echo "Today is the: "
+  date 
+  echo scripting is automation
+  echo "Thanks for coming"
